2241e7c7300757db21a55c0aa1f9f6beb343e367
Nikesh Gupta <nikeshgupta130@gmail.com>
2024-06-26 09:42:45 +0530

    configuration of DB Gitignore and prettier

---
+++ server/src/db/index.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constant.js";
+
+
+const connectDB= async ()=>{
+    try {
+      const ConnectionInstance=  await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+      console.log(`Mongo DB connection Host !!! ${ConnectionInstance.connection.host}`);
+    } catch (error) {
+        console.log("MongoDB Connection Error",error);
+        process.exit(1)
+    }
+}
+
+export default  connectDB
