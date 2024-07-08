8401779d041048a4d5aeeed1c04196b6606fb91d
Nikesh Gupta <nikeshgupta130@gmail.com>
2024-07-08 07:10:59 +0530

    Deleted

--- server/src/routes/product.routes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { upload } from "../middlewares/multer.middleware.js";
-import { addProduct, getProducts } from "../controllers/product/product.controller.js";
-const router = Router();
-
-router.route("/addproduct").post(upload.fields([{name:"thumbnail",maxCount:5},,{name:"customShadeImageUrl",maxCount:5}]),addProduct);
-router.route("/products").get(getProducts)
-
-export default router
--- server/src/utils/asyncHandler.js
+++
@@ -1,7 +0,0 @@
-const asyncHandler =(requestHandler)=>{
-    return (res,req,next)=>{
-        Promise.resolve(requestHandler(res,req,next)).catch((err)=>next(err))
-    }
-}
-
-export {asyncHandler}
